ba7ec741e1cfdf0e60144b0ca2980a534cae4e8d
2ayushtiw2008-cmyk <2ayush.tiw.2008@gmail.com>
2026-09-02 14:39:39 +0100

    Update black_scholes.py

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -2,11 +2,9 @@
 import math
 from scipy.stats import norm
 def black_scholes(S,K,T,r,sigma):
-  d1=(math.log(S/K)+T*(r+(sigma**2)/2))/(sigma*math.sqrt(T))
-  d2=d1-sigma*math.sqrt(T)
+  d1= (math.log(S/K) + T * (r + (sigma**2)/2)) / (sigma*math.sqrt(T))
+  d2= d1 - sigma*math.sqrt(T)
   Nd1= norm.cdf(d1)
   Nd2= norm.cdf(d2)
-  call_price=S*Nd1-K*(math.exp(-r*T))*Nd2
+  call_price= S * Nd1 - K * (math.exp(-r*T)) * Nd2
   return call_price
-price=black_scholes(100, 100, 1, 0.05, 0.2)
-print(price)
