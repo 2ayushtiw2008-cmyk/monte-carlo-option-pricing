d0edf3eb489b742fce0d7a11a9cb5b0adb649c1b
2ayushtiw2008-cmyk <2ayush.tiw.2008@gmail.com>
2026-09-04 17:59:21 +0100

    Update Volatility_Testing.py

--- tests/Volatility_Testing.py
+++ tests/Volatility_Testing.py
@@ -113,3 +113,86 @@ plt.ylabel("Mean Absolute Error")
 plt.title("Effect of Volatility on Monte Carlo Pricing Error")
 
 plt.show()
+maturity_values = [0.08, 0.25, 0.5, 1, 2]
+
+runs = 20
+
+maturity_results = []
+
+
+# Testing different times to maturity
+
+for T in maturity_values:
+
+    # Calculate Black-Scholes price
+    bs_price = black_scholes(S, K, T, r, sigma)
+
+    mc_prices = []
+    errors = []
+
+    # Start timer
+    start_time = time.perf_counter()
+
+    # Repeat Monte Carlo 20 times
+    for i in range(runs):
+
+        mc_price = monte_carlo(S, K, T, r, sigma, N)
+
+        mc_prices.append(mc_price)
+
+        # Calculate error
+        error = abs(mc_price - bs_price)
+
+        errors.append(error)
+
+    # End timer
+    end_time = time.perf_counter()
+
+    # Calculate averages
+    average_mc_price = numpy.mean(mc_prices)
+
+    mean_error = numpy.mean(errors)
+
+    std_error = numpy.std(errors)
+
+    # Average runtime per Monte Carlo run
+    average_runtime = (end_time - start_time) / runs
+
+    maturity_results.append([T,bs_price,average_mc_price,mean_error,std_error,average_runtime])
+
+
+# ============================================================
+# PRESENTING MATURITY DATA
+# ============================================================
+
+maturity_df = pandas.DataFrame(
+    maturity_results,columns=["Time to Maturity","Black-Scholes Price","Average Monte Carlo Price","Mean Absolute Error","Error Standard Deviation","Average Runtime"])
+
+print("\nTIME TO MATURITY EXPERIMENT")
+print(maturity_df)
+
+# ============================================================
+# MATURITY GRAPH 1: TIME → OPTION PRICE
+# ============================================================
+
+plt.figure()
+plt.plot(maturity_df["Time to Maturity"],maturity_df["Black-Scholes Price"],marker="o",label="Black-Scholes")
+plt.plot(maturity_df["Time to Maturity"],maturity_df["Average Monte Carlo Price"],marker="o",label="Monte Carlo")
+plt.xlabel("Time to Maturity (Years)")
+plt.ylabel("Option Price")
+plt.title("Effect of Time to Maturity on European Call Option Price")
+plt.legend()
+
+plt.show()
+
+# ============================================================
+# MATURITY GRAPH 2: TIME → MONTE CARLO ERROR
+# ============================================================
+
+plt.figure()
+plt.plot(maturity_df["Time to Maturity"],maturity_df["Mean Absolute Error"],marker="o")
+plt.xlabel("Time to Maturity (Years)")
+plt.ylabel("Mean Absolute Error")
+plt.title("Effect of Time to Maturity on Monte Carlo Pricing Error")
+
+plt.show()
