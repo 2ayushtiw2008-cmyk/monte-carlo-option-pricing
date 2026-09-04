f32f6e4cc05cfcbd31a8cfc5daf1e3382ba8c166
2ayushtiw2008-cmyk <2ayush.tiw.2008@gmail.com>
2026-09-04 09:06:12 +0100

    Implement Black-Scholes formula for call option pricing

--- src/experiments.py
+++ src/experiments.py
@@ -1,1 +1,13 @@
+#Black Scholes formula
+
+import math
+from scipy.stats import norm
+def black_scholes(S,K,T,r,sigma):
+  d1= (math.log(S/K) + T * (r + (sigma**2)/2)) / (sigma*math.sqrt(T))
+  d2= d1 - sigma*math.sqrt(T)
+  Nd1= norm.cdf(d1)
+  Nd2= norm.cdf(d2)
+  call_price= S * Nd1 - K * (math.exp(-r*T)) * Nd2
+  return call_price
+
 
